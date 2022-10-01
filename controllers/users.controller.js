f92bc619a6eaebb7a7d96e144f3b3eaa3a088b86
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-10-01 12:50:50 +0700

    #5 Login  account with role

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,4 +1,4 @@
-const { registrationAuth } = require("../utils/Auth");
+const { registrationAuth, loginAuth } = require("../utils/Auth");
 
 const registerUser = async (req, res) => {
   await registrationAuth(req.body, "user", res);
@@ -12,4 +12,23 @@ const registerSuperAdmin = async (req, res) => {
   await registrationAuth(req.body, "superadmin", res);
 };
 
-module.exports = { registerUser, registerAdmin, registerSuperAdmin };
+const loginUser = async (req, res) => {
+  await loginAuth(req.body, "user", res);
+};
+
+const loginAdmin = async (req, res) => {
+  await loginAuth(req.body, "user", res);
+};
+
+const loginSuperadmin = async (req, res) => {
+  await loginAuth(req.body, "user", res);
+};
+
+module.exports = {
+  registerUser,
+  registerAdmin,
+  registerSuperAdmin,
+  loginUser,
+  loginAdmin,
+  loginSuperadmin,
+};
--- routes/users.router.js
+++ routes/users.router.js
@@ -3,6 +3,9 @@ const {
   registerUser,
   registerAdmin,
   registerSuperAdmin,
+  loginUser,
+  loginAdmin,
+  loginSuperadmin,
 } = require("../controllers/users.controller");
 
 const usersRouter = express.Router();
@@ -10,5 +13,8 @@ const usersRouter = express.Router();
 usersRouter.route("/register-user").post(registerUser);
 usersRouter.route("/register-admin").post(registerAdmin);
 usersRouter.route("/register-superadmin").post(registerSuperAdmin);
+usersRouter.route("/login-user").post(loginUser);
+usersRouter.route("/login-admin").post(loginAdmin);
+usersRouter.route("/login-superadmin").post(loginSuperadmin);
 
 module.exports = usersRouter;
